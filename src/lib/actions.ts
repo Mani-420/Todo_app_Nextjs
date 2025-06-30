e920f363462b150e5598690ce4ba549aae2fb824
Mani-420 <tahirabdulrehman46@gmail.com>
2025-06-30 15:44:18 +0500

    Actions

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -23,3 +23,34 @@ export async function addTodo(formdata: FormData) {
     completed: false
   });
 }
+
+export async function toggleTodo(id: string) {
+  const { userId } = auth();
+  if (!userId) throw new Error('Unauthorized');
+
+  await connectDB();
+  const todo = await Todo.findOne({ _id: id, userId });
+  if (!todo) return;
+
+  todo.completed = !todo.completed;
+  await todo.save();
+}
+
+export async function deleteTodo(id: string) {
+  const { userId } = auth();
+  if (!userId) throw new Error('Unauthorized');
+
+  await connectDB();
+  await Todo.deleteOne({ _id: id, userId });
+}
+
+export async function updateTodo(id: string, formData: FormData) {
+  const { userId } = auth();
+  if (!userId) throw new Error('Unauthorized');
+
+  const title = formData.get('title')?.toString().trim();
+  if (!title) return;
+
+  await connectDB();
+  await Todo.findOneAndUpdate({ _id: id, userId }, { title }, { new: true });
+}
